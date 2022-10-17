e16132b9f77d7d44869e050d70d2b542f01dbcc6
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-10-17 16:50:53 -0300

    Criada rota de teste

---
+++ src/main/java/br/com/juwer/algafoodapi/MyController.java
@@ -0,0 +1,15 @@
+package br.com.juwer.algafoodapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello man";
+    }
+}
