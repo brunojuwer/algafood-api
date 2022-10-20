30483f91356bde8ecf93970f9d5d76871d71b160
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-10-20 17:05:31 -0300

    Teste de configurações com classes e aplication.properties finalizada

--- src/main/java/br/com/juwer/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/br/com/juwer/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,14 +1,23 @@
 package br.com.juwer.algafoodapi.di.notificacao;
 
 import br.com.juwer.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Autowired
+    private NotificadorProperties notificadorProperties;
+
     @Override
     public void notificar(Cliente cliente, String mensagem){
+
+        System.out.println("HOST: " + notificadorProperties.getHostServidor()
+                + "\nPORTA: " + notificadorProperties.getPortaServidor());
+
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
