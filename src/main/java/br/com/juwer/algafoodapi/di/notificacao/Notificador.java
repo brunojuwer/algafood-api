56e21fe58962ed00be8d4781a39f942607968c62
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-10-22 14:54:02 -0300

    Configurada conexão com banco de dados

--- src/main/java/br/com/juwer/algafoodapi/di/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package br.com.juwer.algafoodapi.di.notificacao;
-
-import br.com.juwer.algafoodapi.di.modelo.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
