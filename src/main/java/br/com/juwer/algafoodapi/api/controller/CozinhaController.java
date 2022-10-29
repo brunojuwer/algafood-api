ec7d248c6073f3e55c2713214c52dddbcd4f3c94
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-10-29 18:20:17 -0300

    adicinada dependencia xml e repos

--- src/main/java/br/com/juwer/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/juwer/algafoodapi/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package br.com.juwer.algafoodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,13 @@ public class CozinhaController {
   @Autowired
   private CozinhaRepository cozinhaRepository;
 
-  @GetMapping
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Cozinha> listar(){
     return cozinhaRepository.listar();
+  }
+
+  @GetMapping("/{cozinhaId}")
+  public Cozinha buscar(@PathVariable Long cozinhaId){
+    return cozinhaRepository.buscar(cozinhaId);
   }
 }
