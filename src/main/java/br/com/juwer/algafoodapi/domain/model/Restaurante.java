b5b6ed70b1ab9e0ae034fab40491f96876a05422
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-12-08 15:09:31 -0300

    Diminuição dos selects removendo o eager loading

--- src/main/java/br/com/juwer/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/model/Restaurante.java
@@ -41,6 +41,7 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
+    // @JsonIgnoreProperties("hibernateLazyInitializer")
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
@@ -63,7 +64,7 @@ public class Restaurante {
     @Column(nullable = false)
     private LocalDateTime dataAtualizacao;
 
-    @JsonIgnore
+    // @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
         joinColumns = @JoinColumn(name = "restaurante_id"),
--- src/main/java/br/com/juwer/algafoodapi/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/repository/RestauranteRepository.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,9 @@ import br.com.juwer.algafoodapi.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>,
         RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+
+  @Query("from Restaurante r left join fetch r.cozinha left join fetch r.formasPagamento")
+  List<Restaurante> findAll();
 
   List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
   
