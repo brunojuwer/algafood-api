bd2a4bf3964d90420c4c412028daf416efc14471
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-12-21 18:25:00 -0300

    Added ExceptionHandlers

--- src/main/java/br/com/juwer/algafoodapi/domain/exception/NegocioException.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/exception/NegocioException.java
@@ -11,4 +11,8 @@ public class NegocioException extends RuntimeException {
   public NegocioException(String message) {
     super(message);
   }
+
+  public NegocioException(String message, Throwable causa) {
+    super(message, causa);
+  }
 }
