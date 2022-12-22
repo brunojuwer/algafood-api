e78662c5e60adf74b8fb1bb847104cac7684188c
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-12-22 20:21:34 -0300

    Created file to handle exceptions globaly

---
+++ src/main/java/br/com/juwer/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,47 @@
+package br.com.juwer.algafoodapi.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.juwer.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
+import br.com.juwer.algafoodapi.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+  @ExceptionHandler(EntidadeNaoEncontradaException.class)
+  public ResponseEntity<?> handleEntidadeNaoEncontrada(
+      EntidadeNaoEncontradaException e) {
+    Problema problema = Problema.builder()
+      .dataHora(LocalDateTime.now())
+      .mensagem(e.getMessage()).build();
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+      .body(problema);
+  }
+
+  @ExceptionHandler(NegocioException.class)
+  public ResponseEntity<?> handleNegocioException(NegocioException e) {
+    Problema problema = Problema.builder()
+      .dataHora(LocalDateTime.now())
+      .mensagem(e.getMessage()).build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+      .body(problema);
+  }
+
+  @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+  public ResponseEntity<?> handleHttpMediaTypeNotSupportedException() {
+    Problema problema = Problema.builder()
+      .dataHora(LocalDateTime.now())
+      .mensagem("Tipo de mídia não suportado").build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+      .body(problema);
+  }
+}
