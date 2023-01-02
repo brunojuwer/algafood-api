07e901ffe805c8707fe12df8ca36c326c4955d70
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-02 14:52:04 -0300

    Customized MethodArgumentNotValidException

--- src/main/java/br/com/juwer/algafoodapi/api/exceptionhandler/ProblemType.java
+++ src/main/java/br/com/juwer/algafoodapi/api/exceptionhandler/ProblemType.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+  DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
   MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem Incompreensível"),
   RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
   ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
   ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
   ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
   PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
-  
+
   private String url;
   private String title;
   
