5d2a4084cc3411473249f229584f9315e25980c9
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-03 11:26:51 -0300

    Tests with ValidationMessages

--- src/main/java/br/com/juwer/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/model/Restaurante.java
@@ -47,7 +47,7 @@ public class Restaurante {
     private String nome;
     
     @NotNull
-    @PositiveOrZero
+    @PositiveOrZero(message = "{TaxaFrete.invalida}")
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
