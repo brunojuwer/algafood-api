5eefc32d3c720dd31b3018207a55964275d533f4
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-03 10:39:43 -0300

    Added custom messages at fields erros

--- src/main/java/br/com/juwer/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/juwer/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +34,10 @@ import br.com.juwer.algafoodapi.domain.exception.NegocioException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-  private final String MSG_ERRO_GENERICA_USUARIO_FINAL = 
+  @Autowired
+  private MessageSource messageSource;
+
+  private static final String MSG_ERRO_GENERICA_USUARIO_FINAL = 
     "Ocorreu um erro interno inesperado no sistema. "
   + "Tente novamente e se o problema persistir, entre em contato "
   + "com o administrador do sistema.";
@@ -111,10 +117,14 @@ public ResponseEntity<Object> handleGlobalExceptions(Exception ex, WebRequest re
 
     List<Problem.Field> problemFields = bindingResult.getFieldErrors()
       .stream()
-      .map(fieldError -> Problem.Field.builder()
+      .map(fieldError -> {
+        String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+
+        return Problem.Field.builder()
         .name(fieldError.getField())
-        .userMessage(fieldError.getDefaultMessage())
-        .build())
+        .userMessage(message)
+        .build();
+      }) 
       .collect(Collectors.toList());
 
 
--- src/main/java/br/com/juwer/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/model/Restaurante.java
@@ -18,6 +18,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
@@ -45,7 +46,7 @@ public class Restaurante {
     @Column(nullable = false)
     private String nome;
     
-    
+    @NotNull
     @PositiveOrZero
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
