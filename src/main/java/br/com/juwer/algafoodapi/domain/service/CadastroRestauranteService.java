66b1553b1f0173a67ed0576000094a0ddc340023
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-09 15:05:46 -0300

    Created some tests

--- src/main/java/br/com/juwer/algafoodapi/domain/service/CadastroRestauranteService.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/service/CadastroRestauranteService.java
@@ -14,7 +14,8 @@ import br.com.juwer.algafoodapi.domain.repository.RestauranteRepository;
 @Service
 public class CadastroRestauranteService {
   
-  private static final String MSG_RESTAURANTE_EM_USO = "Restaurante de código %d não pode ser removido, pois está em uso";
+  private static final String MSG_RESTAURANTE_EM_USO =
+          "Restaurante de código %d não pode ser removido, pois está em uso";
 
   @Autowired
   private RestauranteRepository restauranteRepository;
