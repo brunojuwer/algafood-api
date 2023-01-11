279dc853a764f52825249192e6aea11a226baeb7
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-11 18:04:58 -0300

    Created new way to repopulate ALGAFOOD_TESTt

--- src/test/java/br/com/juwer/algafoodapi/CadastroCozinhaIT.java
+++ src/test/java/br/com/juwer/algafoodapi/CadastroCozinhaIT.java
@@ -1,5 +1,8 @@
 package br.com.juwer.algafoodapi;
 
+import br.com.juwer.algafoodapi.domain.model.Cozinha;
+import br.com.juwer.algafoodapi.domain.repository.CozinhaRepository;
+import br.com.juwer.algafoodapi.utils.DataBaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.flywaydb.core.Flyway;
@@ -19,6 +22,12 @@ class CadastroCozinhaIT {
 	@Autowired
 	private Flyway flyway;
 
+	@Autowired
+	private DataBaseCleaner dataBaseCleaner;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
 	@LocalServerPort
 	private int port;
 
@@ -27,7 +36,19 @@ class CadastroCozinhaIT {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.basePath = "/cozinhas";
 		RestAssured.port = port;
-		flyway.migrate();
+
+		dataBaseCleaner.clearTables();
+		prepararDados();
+	}
+
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Japonesa");
+		cozinhaRepository.save(cozinha1);
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha2);
 	}
 
 	@Test
@@ -42,14 +63,14 @@ class CadastroCozinhaIT {
 	}
 
 	@Test
-	public void deveconterApenas4Cozinhas_QuandoConsultarCozinhas(){
+	public void deveconterApenas2Cozinhas_QuandoConsultarCozinhas(){
 		RestAssured
 			.given()
 				.accept(ContentType.JSON)
 			.when()
 				.get()
 			.then()
-				.body("", Matchers.hasSize(4)); // verifica se tem quatro itens no banco
+				.body("", Matchers.hasSize(2)); // verifica se tem dois itens no banco
 	}
 
 	@Test
