e9072bd529f2597771122da1ef00cd00cbcf805c
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-13 18:49:55 -0300

    Changing API to use DTO representation

---
+++ src/main/java/br/com/juwer/algafoodapi/api/model/dto/RestauranteDTO.java
@@ -0,0 +1,16 @@
+package br.com.juwer.algafoodapi.api.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class RestauranteDTO {
+
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+    private CozinhaDTO cozinhaDTO;
+}
