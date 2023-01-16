eb0eeeb6fcce127e16e704f597dfe2ffe2753f5f
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-16 11:17:46 -0300

    Refactored method in RestauranteController

--- src/main/java/br/com/juwer/algafoodapi/api/disassembler/RestauranteDTODisassembler.java
+++ src/main/java/br/com/juwer/algafoodapi/api/disassembler/RestauranteDTODisassembler.java
@@ -1,6 +1,7 @@
 package br.com.juwer.algafoodapi.api.disassembler;
 
 import br.com.juwer.algafoodapi.api.model.dto.input.RestauranteDTOInput;
+import br.com.juwer.algafoodapi.domain.model.Cozinha;
 import br.com.juwer.algafoodapi.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +15,14 @@ public class RestauranteDTODisassembler {
 
     public Restaurante convertDTOInputToRestaurante(RestauranteDTOInput restauranteDTOInput) {
         return modelMapper.map(restauranteDTOInput, Restaurante.class);
+    }
+
+    public void copyToDomainObject(RestauranteDTOInput restauranteDTOInput, Restaurante restaurante){
+        // serve para resolver:
+        // org.springframework.orm.jpa.JpaSystemException:
+        // identifier of an instance of br.com.juwer.algafoodapi.domain.model.Cozinha was altered from 1 to 2
+        restaurante.setCozinha(new Cozinha());
+
+        modelMapper.map(restauranteDTOInput, restaurante);
     }
 }
