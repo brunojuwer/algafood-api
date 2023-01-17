232b39e8ca4bfba537cd25580fbfc7aa2fb7234d
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-16 21:33:19 -0300

    Removed mixin standard

--- src/main/java/br/com/juwer/algafoodapi/api/model/mixin/CidadeMixin.java
+++
@@ -1,9 +0,0 @@
-package br.com.juwer.algafoodapi.api.model.mixin;
-
-import br.com.juwer.algafoodapi.domain.model.Estado;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-public abstract class CidadeMixin {
-    @JsonIgnoreProperties(value = "nome", allowGetters = true)
-    private Estado estado;
-}
