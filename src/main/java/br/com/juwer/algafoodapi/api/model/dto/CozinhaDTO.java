adba4cd3d9f6f927c5c0a9ac1e90e664974a1a23
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-01-25 16:11:55 -0300

    Added Projections to Restaurante GET endpoint

--- src/main/java/br/com/juwer/algafoodapi/api/model/dto/CozinhaDTO.java
+++ src/main/java/br/com/juwer/algafoodapi/api/model/dto/CozinhaDTO.java
@@ -1,5 +1,7 @@
 package br.com.juwer.algafoodapi.api.model.dto;
 
+import br.com.juwer.algafoodapi.api.model.dto.view.RestauranteView;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,9 @@ import lombok.Setter;
 @Setter
 public class CozinhaDTO {
 
+    @JsonView(RestauranteView.Resumo.class)
     private Long id;
+
+    @JsonView(RestauranteView.Resumo.class)
     private String nome;
 }
--- src/main/java/br/com/juwer/algafoodapi/api/model/dto/RestauranteDTO.java
+++ src/main/java/br/com/juwer/algafoodapi/api/model/dto/RestauranteDTO.java
@@ -1,5 +1,7 @@
 package br.com.juwer.algafoodapi.api.model.dto;
 
+import br.com.juwer.algafoodapi.api.model.dto.view.RestauranteView;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +11,18 @@ import java.math.BigDecimal;
 @Setter
 public class RestauranteDTO {
 
+    @JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNomeEId.class})
     private Long id;
+
+    @JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNomeEId.class})
     private String nome;
+
+    @JsonView(RestauranteView.Resumo.class)
     private BigDecimal taxaFrete;
+
+    @JsonView(RestauranteView.Resumo.class)
     private CozinhaDTO cozinha;
+
     private Boolean ativo;
     private Boolean aberto;
     private EnderecoDTO endereco;
