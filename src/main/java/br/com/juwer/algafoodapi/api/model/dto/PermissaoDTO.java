678cf54bf8958abbbeb22a36d10fac78f9ba7691
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-03-02 17:34:27 -0300

    doc: added docs to GrupoPermissao

--- src/main/java/br/com/juwer/algafoodapi/api/model/dto/PermissaoDTO.java
+++ src/main/java/br/com/juwer/algafoodapi/api/model/dto/PermissaoDTO.java
@@ -1,5 +1,6 @@
 package br.com.juwer.algafoodapi.api.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,12 @@ import lombok.Setter;
 @Setter
 public class PermissaoDTO {
 
+    @ApiModelProperty(example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "EDITAR_COZINHAS")
     private String nome;
+
+    @ApiModelProperty(example = "Permite editar cozinhas")
     private String descricao;
 }
