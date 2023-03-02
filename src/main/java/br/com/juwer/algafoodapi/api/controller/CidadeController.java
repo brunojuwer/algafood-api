a77785fc4b265920c89d90424cfa57719b63a3f2
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-03-01 21:27:58 -0300

    doc: Created Interface for CidadeController
    Created Interface CidadeControllerOpenApi with same methods signature of
    CidadeController class.
    Docs annotaions were tranferred to interface.
    Implemented CidadeControllerOpenApi in CidadeController class

--- src/main/java/br/com/juwer/algafoodapi/api/controller/CidadeController.java
+++ src/main/java/br/com/juwer/algafoodapi/api/controller/CidadeController.java
@@ -1,40 +1,27 @@
 package br.com.juwer.algafoodapi.api.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
 import br.com.juwer.algafoodapi.api.assembler.CidadeDTOAssembler;
+import br.com.juwer.algafoodapi.api.controller.openapi.CidadeControllerOpenApi;
 import br.com.juwer.algafoodapi.api.disassembler.CidadeDTODisassembler;
 import br.com.juwer.algafoodapi.api.model.dto.CidadeDTO;
 import br.com.juwer.algafoodapi.api.model.dto.input.CidadeDTOInput;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.juwer.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import br.com.juwer.algafoodapi.domain.exception.NegocioException;
 import br.com.juwer.algafoodapi.domain.model.Cidade;
 import br.com.juwer.algafoodapi.domain.repository.CidadeRepository;
 import br.com.juwer.algafoodapi.domain.service.CadastroCidadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@Api(tags = "Cidades")
+import javax.validation.Valid;
+import java.util.List;
+
+
 @RestController
 @RequestMapping(value = "/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
-public class CidadeController {
+public class CidadeController implements CidadeControllerOpenApi {
   
   @Autowired
   private CidadeRepository cidadeRepository;
@@ -48,25 +35,19 @@ public class CidadeController {
   @Autowired
   private CidadeDTODisassembler cidadeDTODisassembler;
 
-  @ApiOperation(value = "Listar as cidades")
   @GetMapping
   public List<CidadeDTO> listar(){
     return cidadeDTOAssembler.toCollectionModel(cidadeRepository.findAllCidades());
   }
 
-  @ApiOperation(value = "Buscar uma cidade por ID")
   @GetMapping(value = "/{cidadeId}")
-  private CidadeDTO buscar(@ApiParam(value = "ID de uma cidade") @PathVariable Long cidadeId) {
+  public CidadeDTO buscar(@PathVariable Long cidadeId) {
     return cidadeDTOAssembler.toModel(cadastroCidadeService.buscaOuFalha(cidadeId));
   }
 
-  @ApiOperation(value = "Cadastrar uma cidade")
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public CidadeDTO adicionar(
-          @ApiParam(name = "Corpo", value = "Representação de uma nova cidade")
-          @RequestBody @Valid CidadeDTOInput cidadeDTOInput)
-  {
+  public CidadeDTO adicionar(@RequestBody @Valid CidadeDTOInput cidadeDTOInput) {
     try {
       Cidade cidade = cidadeDTODisassembler.toDomainObject(cidadeDTOInput);
       return cidadeDTOAssembler.toModel(cadastroCidadeService.salvar(cidade));
@@ -75,14 +56,8 @@ public class CidadeController {
     }
   }
 
-  @ApiOperation(value = "Atualizar uma cidade por ID")
   @PutMapping("/{cidadeId}")
-  public CidadeDTO atualizar(
-          @ApiParam(value = "ID de uma cidade")
-          @PathVariable Long cidadeId,
-          @ApiParam(name="corpo", value = "Representação de uma cidade com os novos dados")
-          @RequestBody @Valid CidadeDTOInput cidadeDTOInput)
-  {
+  public CidadeDTO atualizar(@PathVariable Long cidadeId, @RequestBody @Valid CidadeDTOInput cidadeDTOInput) {
     Cidade cidadeAtual = cadastroCidadeService.buscaOuFalha(cidadeId);
     cidadeDTODisassembler.copyToDomainObject(cidadeDTOInput, cidadeAtual);
 
@@ -93,10 +68,9 @@ public class CidadeController {
     }                  
   }
 
-  @ApiOperation(value = "Deletar uma cidade por ID")
   @DeleteMapping("/{cidadeId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void remover(@ApiParam(value = "ID de uma cidade")  @PathVariable Long cidadeId) {
+  public void remover(@PathVariable Long cidadeId) {
     cadastroCidadeService.excluir(cidadeId);
   }
 }
---
+++ src/main/java/br/com/juwer/algafoodapi/api/controller/openapi/CidadeControllerOpenApi.java
@@ -0,0 +1,36 @@
+package br.com.juwer.algafoodapi.api.controller.openapi;
+
+import br.com.juwer.algafoodapi.api.model.dto.CidadeDTO;
+import br.com.juwer.algafoodapi.api.model.dto.input.CidadeDTOInput;
+import io.swagger.annotations.*;
+
+import java.util.List;
+
+@Api(tags = "Cidades")
+public interface CidadeControllerOpenApi {
+
+    @ApiOperation(value = "Listar as cidades")
+     List<CidadeDTO> listar();
+
+    @ApiOperation(value = "Buscar uma cidade por ID")
+    CidadeDTO buscar(@ApiParam(value = "ID de uma cidade") Long cidadeId);
+
+    @ApiOperation(value = "Cadastrar uma cidade")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cidade criada"),
+
+    })
+    CidadeDTO adicionar(
+            @ApiParam(name = "Corpo", value = "Representação de uma nova cidade")
+            CidadeDTOInput cidadeDTOInput);
+
+    @ApiOperation(value = "Atualizar uma cidade por ID")
+    CidadeDTO atualizar(
+            @ApiParam(value = "ID de uma cidade")
+            Long cidadeId,
+            @ApiParam(name="corpo", value = "Representação de uma cidade com os novos dados")
+            CidadeDTOInput cidadeDTOInput);
+
+    @ApiOperation(value = "Deletar uma cidade por ID")
+    void remover(@ApiParam(value = "ID de uma cidade") Long cidadeId);
+}
