528a2e7888403bd686d9d3ae494f9898a6492d03
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-03-06 17:16:29 -0300

    fix: multiples selects when GET usuarios/id/grupos
    Created a native query with joins to avoid that JPA create more than
    one SELECT
    
    closes #162

--- src/main/java/br/com/juwer/algafoodapi/api/controller/UsuarioGruposController.java
+++ src/main/java/br/com/juwer/algafoodapi/api/controller/UsuarioGruposController.java
@@ -4,7 +4,9 @@ package br.com.juwer.algafoodapi.api.controller;
 import br.com.juwer.algafoodapi.api.assembler.GrupoDTOAssembler;
 import br.com.juwer.algafoodapi.api.model.dto.GrupoDTO;
 import br.com.juwer.algafoodapi.api.openapi.controller.UsuarioGruposControllerOpenApi;
+import br.com.juwer.algafoodapi.domain.model.Grupo;
 import br.com.juwer.algafoodapi.domain.model.Usuario;
+import br.com.juwer.algafoodapi.domain.repository.GrupoRepository;
 import br.com.juwer.algafoodapi.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,11 +25,17 @@ public class UsuarioGruposController implements UsuarioGruposControllerOpenApi {
     @Autowired
     private GrupoDTOAssembler grupoDTOAssembler;
 
+    @Autowired
+    private GrupoRepository grupoRepository;
+
     @Override
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<GrupoDTO> listar(@PathVariable Long usuarioId) {
-        Usuario usuario = cadastroUsuarioService.buscaOuFalha(usuarioId);
-        return grupoDTOAssembler.toCollectionModel(usuario.getGrupos());
+//        Usuario usuario = cadastroUsuarioService.buscaOuFalha(usuarioId);
+//        return grupoDTOAssembler.toCollectionModel(usuario.getGrupos());
+        List<Grupo> grupos = grupoRepository.findGruposByUsusarioId(usuarioId);
+
+        return grupoDTOAssembler.toCollectionModel(grupos);
     }
 
     @Override
--- src/main/java/br/com/juwer/algafoodapi/domain/repository/UsuarioRepository.java
+++ src/main/java/br/com/juwer/algafoodapi/domain/repository/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package br.com.juwer.algafoodapi.domain.repository;
 
+import br.com.juwer.algafoodapi.domain.model.Grupo;
 import br.com.juwer.algafoodapi.domain.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
