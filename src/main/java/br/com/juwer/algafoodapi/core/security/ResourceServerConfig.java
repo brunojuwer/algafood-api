bc4bd2998762d9c1055e3947ee80fa7af8244e78
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-05-04 16:52:43 -0300

    Finished stateful authentications

--- src/main/java/br/com/juwer/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/juwer/algafoodapi/core/security/ResourceServerConfig.java
@@ -20,8 +20,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .anyRequest().authenticated()
             .and()
+                .cors()
+            .and()
                 .oauth2ResourceServer().opaqueToken();
-
-
     }
 }
--- src/main/java/br/com/juwer/algafoodapi/core/web/WebConfig.java
+++ src/main/java/br/com/juwer/algafoodapi/core/web/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:1234")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("*");
     }
 
