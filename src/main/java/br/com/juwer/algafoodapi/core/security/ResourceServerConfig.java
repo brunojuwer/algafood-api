57ef6ad77a73148d8436f4d2ac49878a68673bda
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-05-05 16:29:33 -0300

    Added public key

--- src/main/java/br/com/juwer/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/juwer/algafoodapi/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package br.com.juwer.algafoodapi.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -23,15 +18,5 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .cors()
             .and()
                 .oauth2ResourceServer().jwt(); // alterar para jwt()
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        // OBS: chave tem que ter mais de 256 bits
-        var secretKey = new SecretKeySpec(
-                "n26247586h4576h28475h60824576hdfxg807hdfg87".getBytes(),
-                "HmacSHA256");
-
-        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }
