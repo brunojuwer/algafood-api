8795e1941b6107283f44ca1c2f89912652956f52
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-05-23 16:14:07 -0300

    Descommented S3 implementation to use in prod

--- src/main/java/br/com/juwer/algafoodapi/core/storage/StorageConfig.java
+++ src/main/java/br/com/juwer/algafoodapi/core/storage/StorageConfig.java
@@ -3,6 +3,11 @@ package br.com.juwer.algafoodapi.core.storage;
 import br.com.juwer.algafoodapi.domain.service.FotoStorageService;
 import br.com.juwer.algafoodapi.infrastructure.service.storage.LocalFotoStorageService;
 import br.com.juwer.algafoodapi.infrastructure.service.storage.S3FotoStorageService;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,20 +18,20 @@ public class StorageConfig {
     @Autowired
     private StorageProperties storageProperties;
 
-//    @Bean
-//    public AmazonS3 amazonS3() {
-//        String accessIdKey = storageProperties.getS3().getIdKeyAccess();
-//        String secret = storageProperties.getS3().getSecret();
-//        Regions region = storageProperties.getS3().getRegion();
-//
-//        var credentials = new BasicAWSCredentials(accessIdKey, secret);
-//
-//        return AmazonS3ClientBuilder
-//                .standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//                .withRegion(region)
-//                .build();
-//    }
+    @Bean
+    public AmazonS3 amazonS3() {
+        String accessIdKey = storageProperties.getS3().getIdKeyAccess();
+        String secret = storageProperties.getS3().getSecret();
+        Regions region = storageProperties.getS3().getRegion();
+
+        var credentials = new BasicAWSCredentials(accessIdKey, secret);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
 
     @Bean
     public FotoStorageService fotoStorageService() {
